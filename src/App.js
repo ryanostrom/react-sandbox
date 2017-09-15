cf29d176bb6045651585d579683d6cd19d55c515
Ryan Ostrom <ryan.ostrom@praxent.com>
2017-09-14 19:51:38 -0500

    add directory structure
    Signed-off-by: Ryan Ostrom <ryan.ostrom@praxent.com>

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
+import { Switch, BrowserRouter, Route, browserHistory} from 'react-router-dom';
 
-class App extends Component {
+import './App.css'
+
+import Index from './components/index.js'
+
+export default class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Provider store={ createStore(reducers) }>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Index} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    )
   }
-}
 
-export default App;
+}
